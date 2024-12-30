5607640ad995debfc5ff456d9703b24610f3ca0c
Saif <135821652+Safey11@users.noreply.github.com>
2024-12-30 02:15:06 -0800

    update

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -11,9 +11,7 @@ export const PostsList = () => {
                 <Post author="saif" comment="i am a software devloper" />
                 <Post author="adnan" comment="hahahahahahahahaha" />
                 <Post author="waseem" comment="I am a frontend developer" />
-                <Post author="waseem" comment="I am a frontend developer" />
-                <Post author="waseem" comment="I am a frontend developer" />
-                <Post author="waseem" comment="I am a frontend developer" />
+              
             </ul>
         </div>
     )
