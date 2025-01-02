bc07df2a1ac05a2e464405a2bc1d974c0efd7431
waseembaloch21 <154684340+waseembaloch21@users.noreply.github.com>
2025-01-02 15:46:59 +0500

    code done

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Post from './Post'
 import styles from "./PostsList.module.css"
+import NewPost from './NewPost'
+
 
 export const PostsList = () => {
+
+    const [onComment,setOnComment] = useState("")
+    const [onAuthor,setOnAuthor] = useState('')
+
+    function onCommentHandler(event){
+        setOnComment(event.target.value)
+    }
+
+    function onAuthorHandler(event){
+        setOnAuthor(event.target.value)
+    }
+
     return (
         <div >
             <ul className={styles.ul}>
-                <Post author="saif" comment="i am a software devloper" />
+                <NewPost onAuthorChange={onAuthorHandler} onCommentChange={onCommentHandler}   />
+                <Post author={onAuthor} comment={onComment} />
                 <Post author="adnan" comment="hahahahahahahahaha" />
                 <Post author="waseem" comment="I am a frontend developer" />
-                <Post author="waseem" comment="I am a frontend developer" />
-                <Post author="waseem" comment="I am a frontend developer" />
-                <Post author="waseem" comment="I am a frontend developer" />
+              
             </ul>
         </div>
     )
